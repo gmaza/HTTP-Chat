9a83634c4e2deba7840ddc107894c67aa5bcb3a2
gmaza <gmaza13@freeuni.edu.ge>
2020-08-27 03:47:58 +0400

    add httpserver libraries

---
+++ ChatServer/app/src/main/java/com/example/chatserver/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.chatserver
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.net.InetSocketAddress
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
