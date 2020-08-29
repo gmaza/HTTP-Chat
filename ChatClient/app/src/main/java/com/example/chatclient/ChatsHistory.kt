cb4999ebc89c85998694e43d0167bd9dba9b2300
anniemargvela <anmar13@freeuni.edu.ge>
2020-08-29 04:35:11 +0400

    loading/register/collapsingtoolbar

--- ChatClient/app/src/main/java/com/example/chatclient/ChatsHistory.kt
+++ ChatClient/app/src/main/java/com/example/chatclient/ChatsHistory.kt
@@ -4,13 +4,12 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.WindowManager
 
-class MainActivity : AppCompatActivity() {
+class ChatsHistory : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
 
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
-
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_chats_history)
     }
 }
