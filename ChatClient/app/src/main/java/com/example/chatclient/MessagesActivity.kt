d71b6c17b47e35c08b728572e06ec57abea8977d
anniemargvela <anmar13@freeuni.edu.ge>
2020-08-29 19:06:28 +0400

    chat design

---
+++ ChatClient/app/src/main/java/com/example/chatclient/MessagesActivity.kt
@@ -0,0 +1,19 @@
+package com.example.chatclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.WindowManager
+import kotlinx.android.synthetic.main.activity_messages.*
+
+class MessagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        setContentView(R.layout.activity_messages)
+
+        toolbarh.setNavigationIcon(R.drawable.delete_icon)
+
+    }
+}
