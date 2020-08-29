0d4be9c96414511dc1008056202ed1b02ba17ffa
anniemargvela <anmar13@freeuni.edu.ge>
2020-08-29 10:34:12 +0400

    last message time formatting

---
+++ ChatClient/app/src/main/java/com/example/chatclient/utils/Extensions.kt
@@ -0,0 +1,35 @@
+package com.example.chatclient.utils
+
+import java.text.SimpleDateFormat
+import java.util.*
+
+fun Long.lastMessageDate():String {
+    val tmp = this // 'this' corresponds to the list
+
+    val lastMsgDate = Date(this)
+
+    val currentDate = Date()
+
+    val diff: Long = currentDate.getTime() - lastMsgDate.getTime()
+    val seconds = diff / 1000
+    val minutes = seconds / 60
+    val hours = minutes / 60
+    val days = hours / 24
+    if (days<1) {
+        if (hours<1)  {
+            if (minutes < 2) return "1 min"
+            return (minutes.toString()+ " min")
+        }
+        return  (hours.toString()+ " hour")
+
+    }
+
+    val pattern = "dd/MM/yyyy"
+    val simpleDateFormat = SimpleDateFormat(pattern)
+    return (simpleDateFormat.format(lastMsgDate))
+
+
+
+
+
+}
